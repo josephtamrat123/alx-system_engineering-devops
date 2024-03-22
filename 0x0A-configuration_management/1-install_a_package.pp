2ab0a1cf31047489c41188b1bd93142465356041
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2024-03-23 01:42:57 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-# to install flask from pip3 using Puppet run it with 'puppet apply 1-install_a_package.pp'
-exec {'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0'
+# install puppet-lint -v 2.5.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
