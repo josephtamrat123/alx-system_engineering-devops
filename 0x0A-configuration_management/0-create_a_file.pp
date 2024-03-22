c043b6fb727e3d1ae7807d87303ec041c83a8812
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2024-03-23 01:46:38 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
+# creates a file in /tmp
 
-# to create the file in /tmp using Puppet
 file { '/tmp/school':
-  ensure  => file,
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
